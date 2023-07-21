732df1ff2109d18a76cb2c79c76788bb85f74b33
JoaoFrancisco11 <joaofrancisco.monteiro11@gmail.com>
2023-07-21 13:00:32 -0300

    Project completed

--- backend/app.js
+++ backend/app.js
@@ -2,35 +2,13 @@ const express = require('express');
 const restful = require('node-restful');
 const server = express();
 const mongoose = restful.mongoose;
-const bodyParser = require('body-parser');
-const cors = require('cors');
 
 //Database
 mongoose.Promise = global.Promise;   //Associação a API de promise
 mongoose.connect('mongodb://db//mydb');
 
-//Middlewares
-server.use(bodyParser.urlencoded({extended:true}));
-server.use(bodyParser.json());
-server.use(cors());
-
-//ODM
-const Client = restful.model({
-    name: { type: String, required: true}
-})
-
-//Rest API
-Client.methods(
-    ['get',
-    'post',
-    'put',
-    'delete']
-)
-
-Client.updateOptions({new: true, runValidators: true})
-
-//Routes
-Client.register(server, '/clients');
+//Teste
+server.get('/', (req, res, next) => res.send('backend'));
 
 //Start Server
 server.listen(3000);
